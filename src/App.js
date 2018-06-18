8b8160ce113fb65b58a340a47e7c0b877c01cb32
kirtijalan <kirti.jalan@ril.com>
2018-06-18 23:31:51 +0530

    bootstrapping with redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchMovie from './views/SearchMovie';
 
@@ -9,12 +8,9 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">trouvaille</h1>
+          <h6> ~ Kirti Jalan </h6>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>Movie Checklist </p>
         <SearchMovie />
       </div>
